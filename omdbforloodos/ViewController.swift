c209f8b8ae115a3bc2436b6f7ad9846406a8b5b5
Tayfun Sağdıç <30289904+sagdic@users.noreply.github.com>
2022-10-07 16:18:33 +0300

    Initial Commit

---
+++ omdbforloodos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  omdbforloodos
+//
+//  Created by tayfun on 7.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
